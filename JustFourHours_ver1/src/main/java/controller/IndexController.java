c61eba4ba906a42f1c869ef540a2e15c444b6390
jh <jeongh389@gmail.com>
2015-11-29 20:47:51 +0900

    add a log4j

--- JustFourHours_ver1/src/main/java/controller/IndexController.java
+++ JustFourHours_ver1/src/main/java/controller/IndexController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import model.Book;
 
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,7 +18,7 @@ import service.CustList;
 @Controller
 public class IndexController {
 	
-	 Logger log = Logger.getLogger(this.getClass());
+	 Logger log = Logger.getLogger(IndexController.class);
 	
 	@Autowired 
 	private CustList custList;
@@ -24,10 +26,12 @@ public class IndexController {
 	@RequestMapping("index")
 	public String index(Model model) throws SQLException{
 		
+		PatternLayout pattern = new PatternLayout("%t> [%d{yyyy-MM-dd HH:mm:ss}] [%c{1}] [%L] [%p] %m %n");
 		List<Book> getList = this.custList.getList();
+		log.addAppender(new ConsoleAppender(pattern));
+		log.debug("인터셉터 테스트");
 		
 		model.addAttribute("custList",getList);
-		 log.debug("인터셉터 테스트");
 		
 		return "index";
 	}
